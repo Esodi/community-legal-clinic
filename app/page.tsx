d232fee15344cf06d0076f4553f85d45a6c77d9a
Method Mkoma <gershommethod02@gmail.com>
2025-05-18 15:06:17 +0300

    additional info on sevices

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+// app/page.tsx
+'use client';
+
 import dynamic from 'next/dynamic';
 
 // Dynamically import the App component with SSR disabled
@@ -5,10 +8,6 @@ const AppComponent = dynamic(() => import('@/components/App'), {
   ssr: false,
 });
 
-// Set revalidation time to 1 hour (3600 seconds)
-export const revalidate = 3600;
-
-// This is a server component
 export default function Home() {
   return (
     <div className="min-h-screen">
